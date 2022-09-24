55939813f92ce78ab12183c01916bfd000358306
Dunia Goncalves <dgoncalves@everquote.com>
2022-09-23 21:29:25 -0400

    on click hide and show pop-up

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 // see https://repl.it/@Dotdash/Goodreads-Server-Express for implementation details
-const apiUrl = `https://goodreads-server-express--dotdash.repl.co/search/${term}`;
+// const apiUrl = `https://goodreads-server-express--dotdash.repl.co/search/${term}`;
+
+//TODO: create sliding effect in CSS? and use set timeout to create the 8sec delay before disappearing
+const popUpThanks = document.querySelector(".thanks-pu");
+const popUpCB = document.querySelector(".come-back-pu");
+const thanksButton = document.querySelector(".accept");
+const comeBackButton = document.querySelector(".cancel");
+
+thanksButton.addEventListener("click", () => popUpThanks.classList.toggle("hide"));
+comeBackButton.addEventListener("click", () => popUpCB.classList.toggle("hide"));
+
