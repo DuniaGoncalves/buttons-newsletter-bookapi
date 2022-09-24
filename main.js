a6ed2bd63b0dff8af55d0fa4a377b88a4706fb6d
Dunia Goncalves <dgoncalves@everquote.com>
2022-09-24 17:18:52 -0400

    set up book api

--- main.js
+++ main.js
@@ -10,3 +10,31 @@ const comeBackButton = document.querySelector(".cancel");
 thanksButton.addEventListener("click", () => popUpThanks.classList.toggle("hide"));
 comeBackButton.addEventListener("click", () => popUpCB.classList.toggle("hide"));
 
+
+// BOOK API
+
+const searchBtn = document.querySelector('.search-btn').addEventListener('click', apiRequest);
+const bookInfo = document.querySelector('.book-info');
+
+async function apiRequest() {
+  const term = document.querySelector('.search-input').value
+  try {
+    const response = await fetch(`https://goodreads-server-express--dotdash.repl.co/search/${term}`);
+    const data = await response.json(); 
+    console.log(data);
+
+    const bookArray = data.list;
+    console.log(bookArray);
+
+    bookArray.forEach(function (book) {
+      bookInfo.innerHTML += `
+        <div class="book">
+          <img src="${book.imageUrl}" />                                    
+          <h6>${book.title}</h6>
+          <p>${book.authorName}</p>
+        </div>`;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
